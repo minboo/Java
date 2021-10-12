290a98246ef3ebbc36fd58e018627cef4cb69556
minboo <69302396+minboo@users.noreply.github.com>
2021-10-12 09:45:07 +0800

    Add files via upload

---
+++ Chapter_1/code/DAY1/Hello.java
@@ -0,0 +1,18 @@
+public class Hello{
+	public static void main(String[] args){
+		//System.out.println("名字：某某某\n");
+		//System.out.println("性别：男");
+		//System.out.println("北京四合院");
+		// int i1 = 1;
+		// char c1 = 'a';
+		// System.out.println(i1 + c1);
+		// long l1 = 123l;
+		// int i1 = 123;
+		// char c1 = 'a';
+		System.out.println("你好吗" + 1);
+		System.out.println("你好吗" + 'a');
+		System.out.println('1' + 'a');
+		System.out.println(49 + 'a');
+		
+	}
+}
